3ee0f226041c9454fd6c968ca05e93d40baebd10
Raphael Meissonnier <meissonnier.raphael@gmail.com>
2021-09-16 15:55:44 +0200

    Triangulation.6

--- app/src/main/java/fr/parisnanterre/miage/tdd/Dictionnary.java
+++ app/src/main/java/fr/parisnanterre/miage/tdd/Dictionnary.java
@@ -1,11 +1,16 @@
 package fr.parisnanterre.miage.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Dictionnary {
 
     private String name;
+    private Map<String,String> translations;
 
     public Dictionnary(String mot){
         this.name=mot;
+        this.translations=new HashMap<>();
     }
 
     public String getName(){
@@ -17,14 +22,22 @@ public class Dictionnary {
     }
 
     public boolean isEmpty(){
-        return true;
+        if(this.translations.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public String getTranslation(String s1){
-        return "against";
+
+        return translations.get(s1);
     }
 
-    public void addTranslation(String s1, String s2){}
+    public void addTranslation(String s1, String s2){
+        translations.put(s1, s2);
+    }
 
 
 }
